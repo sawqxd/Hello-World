f2ad1171c5c51c07a9984984a5d66d860f064fc3
Sarah Wooldridge <sarahwooldridge@Sarahs-MacBook-Pro.local>
2018-08-24 13:31:09 -0500

    First Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Sarah Wooldridge on 8/24/18.
+//  Copyright © 2018 Sarah Wooldridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
