fde42577a0080debad0853648dabc04c7ad0cbb3
Sarah Wooldridge <sarahwooldridge@Sarahs-MacBook-Pro.local>
2018-08-24 17:59:43 -0500

    First implementation of requirements

---
+++ Hello World/HelloWroldViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HelloWroldViewController.swift
+//  Hello World
+//
+//  Created by Sarah Wooldridge on 8/24/18.
+//  Copyright © 2018 Sarah Wooldridge. All rights reserved.
+//
+
+import UIKit
+
+class HelloWroldViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        messageLabel.text = "Welcome!"
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func helloTapped(_ sender: Any) {
+        
+        messageLabel.text = "Hello World!"
+    }
+    
+    @IBAction func clearTapped(_ sender: Any) {
+        
+        messageLabel.text = ""
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
